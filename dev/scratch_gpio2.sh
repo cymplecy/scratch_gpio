006c852231fb56a3bdb0b109283dfd6c71e3c0eb
cymplecy <simplecy@googlemail.com>
2013-04-20 17:08:59 +0000

    removed scratch_gpio2.sh

--- dev/scratch_gpio2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Version 0.2 - add in & to allow simulatenous running of handler and Scratch
-#Version 0.3 - change sp launches rsc.sb from "/home/pi/Documents/Scratch Projects"
-#Version 0.4 - 20Mar13 meltwater - change to use provided name for home
-sudo ps aux | grep 'python.*scratch_gpio_handler.py' | grep -v grep | awk '{print $2}' | xargs sudo kill -9 
-sudo python /home/pi/sghdev/scratch_gpio/simplesi_scratch_handler/scratch_gpio_handler.py &
-scratch --document "/home/pi/Documents/Scratch Projects/rsc.sb" &
