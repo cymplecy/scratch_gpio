6cb431b9207f6c85803f8e272236ae3d0a00c1e5
cymplecy <simplecy@googlemail.com>
2013-08-11 00:10:43 +0000

    start addding in othe i2c devices

--- dev/scratch_gpio_handler2.py
+++ dev/scratch_gpio_handler2.py
@@ -1,7 +1,7 @@
 # This code is copyright Simon Walters under GPL v2
 # This code is derived from Pi-Face scratch_handler by Thomas Preston
 # This code now hosted on Github thanks to Ben Nuttall
-Version =  2.803 # 02Aug13
+Version =  2.81 # 10Aug13
 
 
 
@@ -93,6 +93,85 @@ class PiGlow:
 		if not isinstance(value, list):
 			value = [value];
 		self.bus.write_i2c_block_data(self.i2c_addr, reg_addr, value)
+                
+class Compass
+
+    __scales = {
+        0.88: [0, 0.73],
+        1.30: [1, 0.92],
+        1.90: [2, 1.22],
+        2.50: [3, 1.52],
+        4.00: [4, 2.27],
+        4.70: [5, 2.56],
+        5.60: [6, 3.03],
+        8.10: [7, 4.35],
+    }
+
+    def __init__(self,,i2c_bus=1, address=0x1E, gauss=1.3, declination=(0,0)):
+        self.bus = smbus.SMBus(i2c_bus)
+        self.address = address
+
+        (degrees, minutes) = declination
+        self.__declDegrees = degrees
+        self.__declMinutes = minutes
+        self.__declination = (degrees + minutes / 60) * math.pi / 180
+
+        (reg, self.__scale) = self.__scales[gauss]
+        self.bus.write_byte_data(self.address, 0x00, 0x70) # 8 Average, 15 Hz, normal measurement
+        self.bus.write_byte_data(self.address, 0x01, reg << 5) # Scale
+        self.bus.write_byte_data(self.address, 0x02, 0x00) # Continuous measurement
+
+    def declination(self):
+        return (self.__declDegrees, self.__declMinutes)
+
+    def twos_complement(self, val, len):
+        # Convert twos compliment to integer
+        if (val & (1 << len - 1)):
+            val = val - (1<<len)
+        return val
+
+    def __convert(self, data, offset):
+        val = self.twos_complement(data[offset] << 8 | data[offset+1], 16)
+        if val == -4096: return None
+        return round(val * self.__scale, 4)
+
+    def axes(self):
+        data = self.bus.read_i2c_block_data(self.address, 0x00)
+        #print map(hex, data)
+        x = self.__convert(data, 3)
+        y = self.__convert(data, 7)
+        z = self.__convert(data, 5)
+        return (x,y,z)
+
+    def heading(self):
+        (x, y, z) = self.axes()
+        headingRad = math.atan2(y, x)
+        headingRad += self.__declination
+
+        # Correct for reversed heading
+        if headingRad < 0:
+            headingRad += 2 * math.pi
+
+        # Check for wrap and compensate
+        elif headingRad > 2 * math.pi:
+            headingRad -= 2 * math.pi
+
+        # Convert to degrees from radians
+        headingDeg = headingRad * 180 / math.pi
+        degrees = math.floor(headingDeg)
+        minutes = round((headingDeg - degrees) * 60)
+        return (degrees, minutes)
+
+    def degrees(self, (degrees, minutes)):
+        return str(degrees) + "°" + str(minutes) + "'"
+
+    def __str__(self):
+        (x, y, z) = self.axes()
+        return "Axis X: " + str(x) + "\n" \
+               "Axis Y: " + str(y) + "\n" \
+               "Axis Z: " + str(z) + "\n" \
+               "Declination: " + self.degrees(self.declination()) + "\n" \
+               "Heading: " + self.degrees(self.heading()) + "\n"
 
 piglow = None
 try:
@@ -110,6 +189,22 @@ except:
     PIN_USE = array('i',[ 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     print "Unexpected error:", sys.exc_info()[0]
 
+
+compass = None
+try:
+    if GPIO.RPI_REVISION == 1:
+        compass = Compass(0)
+    else:
+        compass = Compass(1)
+    #print piglow
+    PIN_NUM = array('i',[11,12,13,15,16,18,22, 7, 24,26,19,21,23, 8,10])
+    PIN_USE = array('i',[ 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    piglow.update_pwm_values(PiGlow_Values)
+except:
+    print "No PiGlow Detected"
+    PIN_NUM = array('i',[11,12,13,15,16,18,22, 7, 3, 5,24,26,19,21,23, 8,10])
+    PIN_USE = array('i',[ 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    print "Unexpected error:", sys.exc_info()[0]
 
 #  GPIO_NUM = array('i',[17,18,21,22,23,24,25,4,14,15,8,7,10,9])
 PINS = len(PIN_NUM)
