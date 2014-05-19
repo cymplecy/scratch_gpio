e633d866cf4501d5b61082ca56596b23013d1008
matthew venn <matt@mattvenn.net>
2014-05-19 12:28:17 +0100

    start from most recently taken photo, no delay or preview when taking photo, chown photos so user can modify them afterwards

--- sgh_RasPiCamera.py
+++ sgh_RasPiCamera.py
@@ -19,27 +19,42 @@
 Version =  '0.0.1sw2' # 14Apr14 Change to get real user home name as runnign as sudo
 
 
-import os
+import os,glob,pwd,grp
 
 class RasPiCamera:
 
     def __init__(self):
         print "pi camera init"
         self.num = 0
-        realUser = os.getenv("SUDO_USER")
-        self.dir = ("/home/"+ realUser + "/photos/")
+        self.user = os.getenv("SUDO_USER")
+        self.dir = ("/home/"+ self.user + "/photos/")
         try:
             os.mkdir(self.dir)
         except OSError:
             #already exists
             pass
-        print "complete"
+
+        photos = glob.glob(self.dir + '*.jpg')
+        if len(photos):
+            latest_photo = sorted(photos)[-1]
+            latest_photo = latest_photo.replace(self.dir,'')
+            latest_photo = latest_photo.replace('.jpg','')
+            latest_photo_num = int(latest_photo)
+            self.num = latest_photo_num + 1
+
+        print("starting at %d" % self.num)
 
 
     def take_photo(self):
         photo_file = self.dir + str(self.num) + '.jpg'
-        os.system("raspistill -o " + photo_file)
+        os.system("raspistill -n -t 1 -o " + photo_file)
         print "photo taken: " + photo_file
+
+	#chown the photo so it can be modified by the user
+	uid = pwd.getpwnam(self.user).pw_uid
+	#assume group is same name as user
+	gid = grp.getgrnam(self.user).gr_gid
+	os.chown(photo_file, uid, gid)
         self.num += 1
 
 #### end RasPiCamera ###############################################################
