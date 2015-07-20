420394a48543a490f7904d0e4c13d3ad246c3126
cymplecy <simplecy@googlemail.com>
2015-07-20 07:17:29 +0000

    add in live dev testing icons

---
+++ scratchgpio7devplus.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Version 0.2 - add in & to allow simulatenous running of handler and Scratch
+#Version 0.3 - change sp launches rsc.sb from "/home/pi/Documents/Scratch Projects"
+#Version 0.4 - 20Mar13 meltwater - change to use provided name for home
+#Version 1.0 - 29Oct13 sw - change to cd into simplesi_scratch_handler to run servods OK
+sudo pkill -f scratchgpio_handler
+cd /home/pi/sghdev/scratch_gpio
+sudo python scratchgpio_handler7.py &
+scratch --document "/home/pi/Documents/Scratch Projects/rsc.sb" &
