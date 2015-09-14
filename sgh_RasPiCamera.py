b66543f855d35a15575314911ede7c0b9eb09db5
matt venn <matt@mattvenn.net>
2015-09-14 17:03:39 +0100

    chown the new directory to pi:pi, mostly pep8 formatted, more helpful error message

--- sgh_RasPiCamera.py
+++ sgh_RasPiCamera.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # sgh_RasPiCamera - control the raspberry pi camera via scratch
-#Copyright (C) 2014 by Matt Venn
+#Copyright (C) 2015 by Matt Venn
 
 #This program is free software; you can redistribute it and/or
 #modify it under the terms of the GNU General Public License
@@ -16,9 +16,13 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-Version =  '0.0.1sw5' # 22May14 Add Try except arroudn take pic code
+Version = '0.0.1sw5'  # 22May14 Add Try except arroudn take pic code
 
-import os,glob,pwd,grp
+import os
+import glob
+import pwd
+import grp
+
 
 class RasPiCamera:
 
@@ -27,51 +31,46 @@ class RasPiCamera:
         self.num = 0
 
         self.user = os.getenv("SUDO_USER")
-        #get uid/gid so we can chown the photo 
+        # get uid/gid so we can chown the photo
         self.uid = pwd.getpwnam(self.user).pw_uid
         self.gid = grp.getgrnam(self.user).gr_gid
-        self.dir = ("/home/"+ self.user + "/photos/")
+        self.dir = ("/home/" + self.user + "/photos/")
 
         try:
             os.mkdir(self.dir)
+            os.chown(self.dir, self.uid, self.gid)
         except OSError:
-            #already exists
+            # already exists
             pass
 
         photos = glob.glob(self.dir + '*.jpg')
-        #if there are existing photos, find the highest numbered
+        # if there are existing photos, find the highest numbered
         if len(photos):
-            photos = [ x.replace(self.dir,'') for x in photos]
-            photos = [ x.replace('.jpg','') for x in photos]
-            photos = [ int(x) for x in photos ]
+            photos = [x.replace(self.dir, '') for x in photos]
+            photos = [x.replace('.jpg', '') for x in photos]
+            photos = [int(x) for x in photos]
             latest_photo_num = sorted(photos)[-1]
             self.num = latest_photo_num + 1
         print("starting at %d" % self.num)
 
-
     def take_photo(self):
 
         photo_file = self.dir + str(self.num) + '.jpg'
-        #try with raspistill for the RasPi camera board
+        # try with raspistill for the RasPi camera board
         os.system("raspistill -n -t 1 -o " + photo_file)
-    
-        #try with fswebcam for usb devices
+
+        # try with fswebcam for usb devices
         if not os.path.isfile(photo_file):
             os.system("fswebcam -r 1024x768 " + photo_file)
 
-        #change ownership if we have a photo   
+        # change ownership if we have a photo
         if os.path.isfile(photo_file):
             os.chown(photo_file, self.uid, self.gid)
             print "photo taken: " + photo_file
             self.num += 1
-        #otherwise, error message
+        # otherwise, error message
         else:
-            print "Error taking photo - camera probably not correctly fitted"
-
-
+            print("Error taking photo - camera probably not correctly fitted")
+            print("if using a USB webcam, ensure fswebcam is installed")
 
 #### end RasPiCamera ###############################################################
-
-
-
-
