b34213820145803bf9699759e09365067a40deb3
cymplecy <simplecy@googlemail.com>
2017-06-29 17:24:45 +0100

    alpha8 with added change x to y function2

--- sgh_piGPIOExtension.js
+++ sgh_piGPIOExtension.js
@@ -6,6 +6,7 @@ new (function() {
     var websocket
     var sensorSplit 
     var sensorDict = {}
+    var variableDict = {}    
     var pinLookup = {'3' : '2', '5' : '3', '7' : '4', '8' : '14', '10' : '15' , '11' : '17', '12' : '18', '13' : '27','15' : '22', '16' : '23', '18' : '24', '19' : '10', '21' : '9', '22' : '25', '23': '11', '24' : '8', '26' : '7', '27' : '0' , '28' : '1', '29' : '5', '31' : '6', '32' : '12', '33' : '13', '35' : '19', '36' : '16', '37' : '26', '38' : '20', '40' : '21'}
 
     function doConnect() 
@@ -129,11 +130,21 @@ ext.send_joinedBroadcast = function (bmsg1, bmsg2, bmsg3, bmsg4, bmsg5)
 ext.send_variable = function (sgh_var,val) 
     {
         sendMessage('sensor-update "' + sgh_var + '" ' + val);
+        variableDict[sgh_var] = val;
     };    
 ext.send_variable2 = function (sgh_var,val) 
     {
         sendMessage('sensor-update "' + sgh_var + '" ' + val);
     };        
+ext.change_variable = function (sgh_var,val) 
+    {
+        if (!(sgh_var in variableDict)) { 
+            variableDict[sgh_var] = val;
+        } else {
+            variableDict[sgh_var] = (parseFloat(val) + parseFloat(variableDict[sgh_var])).toString();
+        }
+        sendMessage('sensor-update "' + sgh_var + '" ' +  variableDict[sgh_var]);        
+    };    
 ext.set_pin = function (pin,val) 
     {
         sendMessage('broadcast "pin' + pin + val +'"');
@@ -200,6 +211,7 @@ ext.get_cheerlightsSensor = function ()
             ['r', 'pin %m.pin_numbers sensor value', 'get_pin', '7'],   
             [' ', 'broadcast %s', 'send_broadcast', ' '],
             [' ', 'set %s to %s', 'send_variable', '', ''],
+            [' ', 'change %s by %n', 'change_variable', '', ''],            
             ['r', '%s sensor value', 'get_sensorMsg', ''],
             [' ', 'broadcast %s %s %s %s %s', 'send_joinedBroadcast', ' ', ' ', ' ', ' ', ' '],            
             ['b', 'gpio %n is high?', 'get_gpio', ''],
