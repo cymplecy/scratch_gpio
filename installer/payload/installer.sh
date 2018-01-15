29961464fbb04240e5d622caf47188188ff60798
Helen Griffiths <threepistons@users.noreply.github.com>
2018-01-15 17:22:58 +0000

    attempt to use world-readable location for sample docs

--- installer/payload/installer.sh
+++ installer/payload/installer.sh
@@ -121,7 +121,7 @@ echo "python scratchgpio_handler8.py 127.0.0.1 standard &" >> $RDIR/scratchgpio$
 echo "## Give the new gpio handler five seconds to get started fully."  >> $RDIR/scratchgpio${SGHVER}/scratchgpio${SGHVER}.sh
 echo "sleep 5"  >> $RDIR/scratchgpio${SGHVER}/scratchgpio${SGHVER}.sh
 echo "## The double ampersand and all that follows it pkills the user's scratchgpio_handler after Scratch exits." >> $RDIR/scratchgpio${SGHVER}/scratchgpio${SGHVER}.sh
-echo "scratch --document \"\$HOME/Documents/Scratch Projects/rsc.sb\" && pkill -f scratchgpio_handler" >> $RDIR/scratchgpio${SGHVER}/scratchgpio${SGHVER}.sh
+echo "scratch --document \"\$HOME/Documents/Scratch Projects/pibrella.sb\" && pkill -f scratchgpio_handler" >> $RDIR/scratchgpio${SGHVER}/scratchgpio${SGHVER}.sh
 chmod +x $RDIR/scratchgpio${SGHVER}/scratchgpio${SGHVER}.sh
 
 #Create new desktop icon
