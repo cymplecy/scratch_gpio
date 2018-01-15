0a0767fa2c6220d7fe00a01226dc5c31a4d404a8
Helen Griffiths <threepistons@users.noreply.github.com>
2018-01-15 16:47:38 +0000

    alter build.sh to not insist on building in a fixed path

--- installer/build.sh
+++ installer/build.sh
@@ -2,25 +2,25 @@
 #copy files to payload folder
 SGHVER=$1
 echo $SGHVER
-echo $HOME
-echo $SGHVER > $HOME/sghdev/scratch_gpio/installer/payload/SGHVER.txt
+echo $PWD
+echo $SGHVER > payload/SGHVER.txt
 
-cp $HOME/sghdev/scratch_gpio/scratchgpio_handler8.py $HOME/sghdev/scratch_gpio/installer/payload
-cp $HOME/sghdev/scratch_gpio/Adafruit_I2C.py $HOME/sghdev/scratch_gpio/installer/payload
-cp $HOME/sghdev/scratch_gpio/sgh_servod $HOME/sghdev/scratch_gpio/installer/payload
-cp $HOME/sghdev/scratch_gpio/killsgh.sh $HOME/sghdev/scratch_gpio/installer/payload
-cp $HOME/sghdev/scratch_gpio/nunchuck.py $HOME/sghdev/scratch_gpio/installer/payload
-cp $HOME/sghdev/scratch_gpio/meArm.py $HOME/sghdev/scratch_gpio/installer/payload
-cp $HOME/sghdev/scratch_gpio/kinematics.py $HOME/sghdev/scratch_gpio/installer/payload
+cp ../scratchgpio_handler8.py payload
+cp ../Adafruit_I2C.py payload
+cp ../sgh_servod payload
+cp ../killsgh.sh payload
+cp ../nunchuck.py payload
+cp ../meArm.py payload
+cp ../kinematics.py payload
 
 
-cp $HOME/sghdev/scratch_gpio/sgh_*.py $HOME/sghdev/scratch_gpio/installer/payload
+cp ../sgh_*.py payload
 
-rm -rf $HOME/sghdev/scratch_gpio/installer/payload/mcpi
-mkdir -p $HOME/sghdev/scratch_gpio/installer/payload/mcpi
-cp $HOME/sghdev/scratch_gpio/mcpi/* $HOME/sghdev/scratch_gpio/installer/payload/mcpi
+rm -rf payload/mcpi
+mkdir -p payload/mcpi
+cp ../mcpi/* payload/mcpi
 
-cd $HOME/sghdev/scratch_gpio/installer/payload
+cd payload
 tar -cf ../payload.tar ./* #tar all the payload files
 cd ..
 
@@ -39,7 +39,4 @@ else
 fi
 chmod +x install_scratchgpio${SGHVER}.sh #make install script executeable
 echo "install_scratchgpio"$SGHVER".sh created"
-cp install_scratchgpio${SGHVER}.sh $HOME/sghdev/scratch_gpio #copy installer to main folder
-cd $HOME/sghdev/scratch_gpio
-
-exit 0
+cp install_scratchgpio${SGHVER}.sh .. #copy installer to main folder
