3f9817e59f58a797d0010af1a5c24f4a940bba4d
cymplecy <simplecy@googlemail.com>
2018-09-17 11:07:07 +0100

    bug fix PiTop

--- installer/payload/sgh_GPIOController.py
+++ installer/payload/sgh_GPIOController.py
@@ -138,7 +138,7 @@ class GPIOController :
         self.stepperBPos = 0
         self.stepperDPos = 0
 
-        self.validPins =      [ 3,         5,       7, 8,   10,11,  ,13,   15,16,   18,19,   21,22,23,24,   26]
+        self.validPins =      [ 3,         5,        7, 8,   10,11,   13,   15,16,   18,19,   21,22,23,24,   26]
         
         
         if self.piRevision == 1:
