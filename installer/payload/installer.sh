b343adbc0b41acffcce78c3d95da8818ada1febe
cymplecy <simplecy@googlemail.com>
2019-03-01 11:50:04 +0000

    S3GPIO RC1 v3

--- installer/payload/installer.sh
+++ installer/payload/installer.sh
@@ -103,7 +103,7 @@ cp ca.* $RDIR/scratchgpio${SGHVER}
 cp ./S3GPIOExtension/* $RDIR/scratchgpio${SGHVER}/S3GPIOExtension
 chmod 777 $RDIR/scratchgpio${SGHVER}/S3GPIOExtension/*
 #Different approach to other orig ScratchGPIO icons
-cp s3gpiobeta2.sh $RDIR/scratchgpio${SGHVER}
+cp s3gpiorc1.sh $RDIR/scratchgpio${SGHVER}
 chmod +x $RDIR/scratchgpio${SGHVER}/s3gpiorc1.sh
 cp S3GPIORC1.desktop $HDIR/Desktop
 
