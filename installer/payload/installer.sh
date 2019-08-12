5faf7db6be6e744a59ef9be5deb3d956bbf42be0
cymplecy <simplecy@googlemail.com>
2019-08-12 15:03:52 +0100

    properly add in Scratch2

--- installer/payload/installer.sh
+++ installer/payload/installer.sh
@@ -14,6 +14,7 @@
 #Version 4Aug14 - change for ScratchGPIO5
 #Version 17Sep15 debug creating old scratchgpio icons
 #Version 14Apr18 add in dht11
+#Version 8Jul19 Properly integrate Scratch2 support
 
 SGHVER=$(<SGHVER.txt)
 f_exit(){
@@ -173,6 +174,42 @@ echo "Categories=Application;Education;Development;" >>  $HDIR/Desktop/scratchgp
 echo "MimeType=application/x-scratch-project" >>  $HDIR/Desktop/scratchgpio${SGHVER}plus.desktop
 
 chown -R $USERID:$GROUPID  $HDIR/Desktop/scratchgpio${SGHVER}plus.desktop
+
+#Instead of copying the scratch2gpioXplus.sh file, we will generate it
+#Create a new file for scratch2gpioXplus.sh
+echo "#!/bin/bash" > $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "#Version 0.2 - add in & to allow simulatenous running of handler and Scratch" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "#Version 0.3 - change sp launches rsc.sb from \"/home/pi/Documents/Scratch Projects\"" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "#Version 0.4 - 20Mar13 meltwater - change to use provided name for home" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "#Version 1.0 - 29Oct13 sw - change to cd into simplesi_scratch_handler to run servods OK" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "#Version 1.1 - 26Jun17 sw - add in Scratch2GPIO" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "sudo pkill -f scratchgpio_handler" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "sudo pkill -f wstosgh" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "scratch2 &" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "cd $RDIR/scratchgpio"$SGHVER >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "sudo rm /usr/lib/scratch2/scratch_extensions/piGPIOExtension.js" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "sudo cp $RDIR/scratchgpio"$SGHVER"/sgh_piGPIOExtension.js /usr/lib/scratch2/scratch_extensions/piGPIOExtension.js" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "sudo python scratchgpio_handler8.py &" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+echo "python wstosgh.py &" >> $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+
+
+chmod +x $RDIR/scratchgpio${SGHVER}/scratch2gpio${SGHVER}plus.sh
+#chown -R $USERID:$GROUPID $HDIR/scratchgpio${SGHVER}/scratchgpio${SGHVER}plus.sh
+
+#Create new desktop icon for plus version
+echo "[Desktop Entry]" > $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
+echo "Encoding=UTF-8" >>  $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
+echo "Version=1.0" >>  $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
+echo "Type=Application" >>  $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
+echo "Exec="$RDIR"/scratchgpio"$SGHVER"/scratch2gpio"$SGHVER"plus.sh" >>  $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
+echo "Icon=scratch" >>  $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
+echo "Terminal=false" >>  $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
+echo "Name=Scratch2GPIO "$SGHVER"plus" >>  $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
+echo "Comment= Programming system and content development tool" >>  $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
+echo "Categories=Application;Education;Development;" >>  $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
+echo "MimeType=application/x-scratch-project" >>  $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
+
+chown -R $USERID:$GROUPID  $HDIR/Desktop/scratch2gpio${SGHVER}plus.desktop
 
 mkdir -p $HDIR/Desktop/ScratchGPIO\ Extras
 chown -R $USERID:$GROUPID $HDIR/Desktop/ScratchGPIO\ Extras
